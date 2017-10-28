199564629a30cafffc3f90b22a47952e01a071e3
Niko <greece93@gmail.com>
2017-10-28 15:02:55 +0200

    first final ReinforcementLearningAgent

--- main_cardgame.py
+++ main_cardgame.py
@@ -1,4 +1,6 @@
 """ Let the Game begin """
+# Arguments when starting from console: main_cardgame.py [total_games] [Number of X for progress]
+import sys
 from cardgame.random_ai import RandomAIPlayer
 from cardgame.reinforcement_learning_agent import RLAgent
 from cardgame.real_player import RealPlayer
@@ -7,15 +9,16 @@ from cardgame.game import Game
 #p1 = RealPlayer("Niko")
 
 p2 = RandomAIPlayer ("John")
-p3 = RLAgent ("Stefan", hidden_layers=20, learning_rate=1e-2, update_frequency=50)
+p3 = RLAgent ("Stefan", hidden_layers=20, learning_rate=1e-2, update_frequency=10)
 
-total_games = 1000
+total_games = int(sys.argv[1]) if len(sys.argv) > 1 else 50
+progress_splits = int(sys.argv[2]) if len(sys.argv) > 2 else 10
 
 g = Game()
 g.init_game(p2, p3)
 for i in range(total_games):
     g.start_new_match()
-    if i % (total_games / 10) == 0:
+    if i % (total_games / progress_splits) == 0:
         print("x", end='', flush=True)
 
 print("")
